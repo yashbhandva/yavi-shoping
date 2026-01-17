71a64b074d0eb4da909872b9982b0ea808279535
yashbhandva <yashbhandva01@gmail.com>
2026-01-17 23:14:43 +0530

    Update Sidebar.jsx

--- ecom-frontend/src/components/shared/Sidebar.jsx
+++ ecom-frontend/src/components/shared/Sidebar.jsx
@@ -14,29 +14,29 @@ const Sidebar = ({isProfileLayout = false}) => {
     const sideBarLayout = isAdmin ? adminNavigation : sellerNavigation;
     
   return (
-    <div className='flex grow flex-col gap-y-7 overflow-y-auto bg-custom-gradient px-6 pb-4'>
-        <div className='flex h-16 shrink-0 gap-x-3 pt-2'>
-            <FaTachometerAlt className='h-8 w-8 text-indigo-500'/>
-            <h1 className='text-white text-xl font-bold'>
+    <div className='sidebar-container'>
+        <div className='sidebar-header'>
+            <FaTachometerAlt className='header-icon'/>
+            <h1 className='header-title'>
                 {isAdmin ? "Admin Panel" : "Seller Panel"}
             </h1>
         </div>
-        <nav className='flex flex-1 flex-col'>
-            <ul role='list' className='flex flex-1 flex-col gap-y-7'>
+        <nav className='sidebar-nav'>
+            <ul role='list' className='nav-list'>
                 <li>
-                    <ul role='list' className='-mx-2 space-y-4'>
+                    <ul role='list' className='nav-sublist'>
                         {sideBarLayout.map((item) => (
                             <li key={item.name}>
                                 <Link
                                     to={item.href}
                                     className={classNames(
+                                        'nav-link',
                                         pathName === item.href
-                                            ? "bg-custom-blue text-white"
-                                            : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                                            ? "active"
+                                            : "inactive"
                                     )}>
 
-                                        <item.icon className='text-2xl'/>
+                                        <item.icon className='nav-icon'/>
                                         {item.name}
                                 </Link>
                             </li>
